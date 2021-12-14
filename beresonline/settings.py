5b6e86287a5acde13c5adf1a7b51cd279b4d598b
Joseph Lane <jlane.pro@gmail.com>
2021-12-14 10:42:10 -0600

    Initial run

--- beresonline/settings.py
+++ beresonline/settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'beresonline.herokuapp.com']
 
