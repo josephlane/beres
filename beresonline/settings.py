3b00fa7189f188f712ab8ed0b8a801129a055c00
Joseph Lane <jlane.pro@gmail.com>
2021-12-14 11:24:23 -0600

    initial

--- beresonline/settings.py
+++ beresonline/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # location where you will store your static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'beres/static')
 ]
