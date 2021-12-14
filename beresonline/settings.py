926ab4e2785916debedeb02301c89d2db301739a
Joseph Lane <jlane.pro@gmail.com>
2021-12-14 00:42:48 -0600

    initial run

--- beresonline/settings.py
+++ beresonline/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'beres-online.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'beresonline.herokuapp.com']
 
 LOGOUT_REDIRECT_URL = '/beres'
 LOGIN_REDIRECT_URL = '/beres'
