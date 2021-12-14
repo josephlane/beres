f8d90f32586eec299a627acb888f5fb1857c2223
Joseph Lane <jlane.pro@gmail.com>
2021-12-14 11:42:56 -0600

    initial

--- beresonline/settings.py
+++ beresonline/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'beres/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'beres/static'),
+        os.path.join(BASE_DIR,'beresonline/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
